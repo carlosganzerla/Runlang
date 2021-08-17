23bdd4457a801c2ff8df1d5de991102c9c4d8f9d
Carlo Sganzerla <carlosganzrl@hotmail.com>
2021-08-17 19:56:44 -0300

    wip

--- src/Runlang/Workout/WorkoutTree.fs
+++ src/Runlang/Workout/WorkoutTree.fs
@@ -11,55 +11,36 @@ type WorkoutTree =
 
 [<RequireQualifiedAccess>]
 module WorkoutTree =
-    // TODO Optimize this MO'FO
-    // TODO Make this tail recursive, or Create catamorphism if not possible.
-    let toIntervals paceTable tree =
-        let rec loop tree acc =
-            match tree with
-            | Repeat (count, nodes) ->
-                nodes
-                |> flip (List.foldBack loop) []
-                |> List.replicate (int count)
-                |> List.collect id
-                |> flip (List.foldBack <| curry List.Cons) acc
-            | Step step ->
-                step
-                |> WorkoutStep.toIntervals paceTable
-                |> flip (List.foldBack <| curry List.Cons) acc
 
-        loop tree []
+    let rec catamorph fStep fEmpty fSingle fRep tree =
+        let recurse = catamorph fStep fEmpty fSingle fRep
+        match tree with
+        | Repeat (_, [])
+        | Repeat (0u, _) -> fEmpty ()
+        | Repeat (1u, nodes) -> List.map recurse nodes |> fSingle
+        | Repeat (count, nodes) -> nodes |> List.map recurse |> fRep count
+        | Step step -> fStep step
+
+    let toIntervals paceTable tree =
+        let fStep = WorkoutStep.toIntervals paceTable
+        let fRep count intervals =
+            intervals
+            |> List.collect id
+            |> List.replicate (int count)
+            |> List.collect id
+        catamorph fStep fRep tree
 
     let rec toString tree =
         let join (str: string list) = System.String.Join (" + ", str)
-
-        let rec loop tree acc =
-            match tree with
-            | Repeat (count, nodes) ->
-                let innerString =
-                    nodes |> flip (List.foldBack loop) [] |> List.rev |> join
-
-                let repeatString = $"{count}x({innerString})"
-                repeatString :: acc
-            | Step step ->
-                let stepString = WorkoutStep.toString step
-                stepString :: acc
-
-        loop tree [] |> List.rev |> join
-
+        let fStep = WorkoutStep.toString
+        let fRep count steps = $"{count}x({join steps})"
+        catamorph fStep fRep tree
 
     let encode tree =
-        let rec loop tree acc =
-            match tree with
-            | Repeat (_, []) -> acc
-            | Repeat (1u, nodes) -> nodes |> List.fold (flip loop) acc
-            | Repeat (count, nodes) ->
-                let fromIndex = List.length acc
-                let repeatStep = EncodedWorkoutStep.createRepeat fromIndex count
-                let repeatLoop = nodes |> List.fold (flip loop) acc
-                repeatStep :: repeatLoop
-            | Step step ->
-                step
-                |> WorkoutStep.encode
-                |> List.fold (flip <| curry List.Cons) acc
-
-        loop tree [] |> List.rev
+        let fStep (step, index) =
+            let steps = WorkoutStep.encode step
+            (steps, index + List.length steps)
+        let fRep count (stepsLists, index) = 
+            let repeat = EncodedWorkoutStep.createRepeat index count
+            let 
+        catamorph fStep fRep tree |> fst
